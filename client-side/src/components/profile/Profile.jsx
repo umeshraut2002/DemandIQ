ba0e947686ef3ab61805c21c0bf6782d14551587
umesh raut <uraut6044@gmail.com>
2024-12-13 13:10:40 +0530

    profile page updated

--- client-side/src/components/profile/Profile.jsx
+++ client-side/src/components/profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Profile = () = { 
-    return(
+const Profile = () => {
+    return (
         <>
-
+            <H2>HI IAm Profile PAge </H2>
+        </>
     )
 }
 
 export default Profile;
-
